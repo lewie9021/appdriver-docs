5bcfb2d484e5d5ac1309f8c92f36f2df5133ebdf
Lewis Barnes <lb.barnes@live.co.uk>
2020-05-02 15:16:18 +0100

    More changes to site config.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'AppDriver', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://lewie9021.github.io/', // Your website URL
-  baseUrl: '/projects/appdriver/', // Base URL for your project */
+  url: 'https://lewie9021.github.io', // Your website URL
+  baseUrl: '/appdriver-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'appdriver-docs',
-  organizationName: 'Lewie9021',
+  organizationName: 'lewie9021',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
