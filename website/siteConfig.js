2448af0286dad5ee3fa32dc95366aede2efd9868
Lewis Barnes <lb.barnes@live.co.uk>
2020-05-05 07:29:31 +0100

    Formatted config and updated defaults.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,96 +1,33 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
-  title: 'AppDriver', // Title for your website.
-  tagline: 'A mobile focused test automation framework',
-  url: 'https://lewie9021.github.io', // Your website URL
-  baseUrl: '/appdriver/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
-  projectName: 'appdriver',
-  organizationName: 'lewie9021',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-
-  // For no header links in the top nav bar -> headerLinks: [],
+  title: "AppDriver",
+  tagline: "A mobile focused test automation framework",
+  url: "https://lewie9021.github.io",
+  baseUrl: "/appdriver/",
+  projectName: "appdriver",
+  organizationName: "lewie9021",
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: "doc1", label: "Docs"},
+    {doc: "doc4", label: "API"},
+    {page: "help", label: "Help"},
+    {blog: true, label: "Blog"},
   ],
-
-  // If you have users set above, you add it here:
-  users,
-
-  /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
-
-  /* Colors for website */
+  users: [],
+  headerIcon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  favicon: "img/favicon.ico",
   colors: {
-    primaryColor: '#243da6',
-    secondaryColor: '#192a74',
+    primaryColor: "#243da6",
+    secondaryColor: "#192a74",
   },
-
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
-
+  copyright: `Copyright © ${new Date().getFullYear()} Lewis Barnes`,
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default",
   },
-
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
-
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
+  scripts: ["https://buttons.github.io/buttons.js"],
+  onPageNav: "separate",
   cleanUrl: true,
-
-  // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg",
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
@@ -99,12 +36,9 @@ const siteConfig = {
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  repoUrl: "https://github.com/lewie9021/appdriver",
 };
 
 module.exports = siteConfig;
