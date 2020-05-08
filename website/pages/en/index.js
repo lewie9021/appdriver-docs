1355516df4382a2cf6c66ff8a2d658733903cef9
Lewis Barnes <lb.barnes@live.co.uk>
2020-05-08 17:13:54 +0100

    Updated button on home page.

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -64,9 +64,9 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href={docUrl('getting-started.html')}>Get Started</Button>
+            <Button href={docUrl('configuration.html')}>API Reference</Button>
+            <Button href="https://github.com/lewie9021/appdriver-native-demo-app">Native Demo App</Button>
           </PromoSection>
         </div>
       </SplashContainer>
